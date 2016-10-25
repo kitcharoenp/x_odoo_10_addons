fa58eec80cabb30e66758b9807d8de9a4f11555c
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-10-25 16:13:27 +0700

    [IMP] x_hr_timesheet_sheet : verifies x_start_date and x_end_date 	1. verifies if x_start_date is earlier than x_end_date. 	2. verifies if x_start_date is not same date x_end_date.

--- x_hr_timesheet_sheet/models/account_analytic_line.py
+++ x_hr_timesheet_sheet/models/account_analytic_line.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -29,11 +29,25 @@ class AccountAnalyticLine(models.Model):
         string='Date',
         index=True)
 
-# auto change 'date' onchange of 'x_start_date'
     @api.onchange('x_start_date')
     def _compute_date_from_x_start_date(self):
+        """ auto change 'date' onchange of 'x_start_date """
         for ts_line in self:
             if ts_line.x_start_date:
                 ts_line.date = datetime.strptime(
                     ts_line.x_start_date,
                     DEFAULT_SERVER_DATETIME_FORMAT).date()
+
+    @api.constrains('x_start_date', 'x_end_date')
+    def _check_validity_x_start_date_x_end_date(self):
+        """ verifies if x_start_date is earlier than x_end_date.
+            verifies if x_start_date is not same date x_end_date.
+        """
+        for ts_line in self:
+            if ts_line.x_start_date and ts_line.x_end_date:
+                if ts_line.x_end_date < ts_line.x_start_date:
+                    raise ValidationError(_(
+                        '"Start" time cannot be earlier than "End" time.'))
+                if ts_line.x_end_date.date() != ts_line.x_start_date.date():
+                    raise ValidationError(_(
+                        '"Start" time must be the same date "End" time.'))

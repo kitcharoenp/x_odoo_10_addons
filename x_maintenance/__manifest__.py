33a34864fd5f8964b27e2050e5c9635369e9c030
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-11-22 14:47:18 +0700

    [NEW] x_maintenance : initial x_maintenance module

---
+++ x_maintenance/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Extend - Maintenance',
+    'version': '1.0',
+    'sequence': 125,
+    'description': """
+        Extend Maintenance.""",
+    'depends': ['maintenance'],
+    'summary': 'Equipments',
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto-install': True,
+}

d0c58868e9a142790032d1d5ae4b430e9648567d
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-12-06 12:47:06 +0700

    [IMP] x_survey : Decrease a titles and a questions font in the survey web form

---
+++ x_survey/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Enhancement - Survey',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'description': """
+        Enhancement Survey.""",
+    'depends': ['survey'],
+    'summary': 'Decrease the title and question font in survey form',
+    'data': [
+        'views/x_survey_templates.xml',
+    ],
+    'installable': True,
+    'auto-install': True,
+}

21dc5e46ae83932f577cc28b98908db3371b70ce
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-12-22 12:08:48 +0700

    [INI] x_account_withholding_tax_document : Initial the x_account_withholding_tax_document which recorded the withholding tax document

---
+++ x_account_withholding_tax_document/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Withholding Tax Document',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Accounting & Finance',
+    'description': """
+        Withholding Tax Document""",
+    'depends': [],
+    'summary': 'QWeb PDF Withholding Tax Document',
+    'data': [
+    ],
+    'installable': True,
+    'auto-install': True,
+}

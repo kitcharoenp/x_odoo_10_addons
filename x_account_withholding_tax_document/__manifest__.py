8a29b4eeb4e282c86c482e7da80f2c8a1311c975
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-12-23 18:21:07 +0700

    [INI] x_account_withholding_tax_document : Created the withholding tax Qweb-pdf report for print-out

--- x_account_withholding_tax_document/__manifest__.py
+++ x_account_withholding_tax_document/__manifest__.py
@@ -7,9 +7,16 @@
     'category': 'Accounting & Finance',
     'description': """
         Withholding Tax Document""",
-    'depends': [],
-    'summary': 'QWeb PDF Withholding Tax Document',
+    'depends': ['report'],
+    'summary': 'Withholding Tax Document',
     'data': [
+    # Reports
+        'report/x_withholding_tax_document_report.xml',
+    # Report Templates
+        'report/templates/x_withholding_tax_document_template.xml',
+        'report/templates/x_wht_document_company_section_template.xml',
+        'report/templates/x_wht_document_taxpayer_section_template.xml',
+        'report/templates/x_wht_document_income_section_template.xml',
     ],
     'installable': True,
     'auto-install': True,

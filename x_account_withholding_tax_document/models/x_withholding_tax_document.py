d36ca738481af3d7be2aa479c1e6ce7c5270213a
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-12-26 09:55:20 +0700

    [IMP] x_account_withholding_tax_document : add a many2many taxs field and calculate total with tax

--- x_account_withholding_tax_document/models/x_withholding_tax_document.py
+++ x_account_withholding_tax_document/models/x_withholding_tax_document.py
@@ -2,6 +2,8 @@
 
 from odoo import api, fields, models, _
 
+import odoo.addons.decimal_precision as dp
+
 
 class WithholdingTaxDocument(models.Model):
     _name = "withholding.tax.document"
@@ -10,9 +12,11 @@ class WithholdingTaxDocument(models.Model):
     _order = "date desc, id desc"
 
     name = fields.Char(
-        string='Document Reference',
+        string='Sequence',
         required=True,
+        readonly=True,
         index=True,
+        copy=False,
         default=lambda self: _('New'))
     date = fields.Date(
         string="Date Paid",
@@ -26,8 +30,62 @@ class WithholdingTaxDocument(models.Model):
     product_id = fields.Many2one(
         'product.product',
         string='Type of Income',
+        required=True)
+    product_uom_id = fields.Many2one(
+        'product.uom',
+        string='Unit of Measure',
         required=True,
-        domain=[('is_withhoding_tax', '=', True)])
+        default=lambda self: self.env['product.uom'].search(
+            [], limit=1, order='id'))
+    unit_amount = fields.Float(
+        string='Unit Price',
+        required=True,
+        digits=dp.get_precision('Product Price'))
+    quantity = fields.Float(
+        required=True,
+        digits=dp.get_precision('Product Unit of Measure'),
+        default=1)
+    tax_ids = fields.Many2many(
+        'account.tax',
+        string='Taxes')
+    untaxed_amount = fields.Float(
+        string='Subtotal',
+        store=True,
+        compute='_compute_amount',
+        digits=dp.get_precision('Account'))
+    total_amount = fields.Float(
+        string='Total',
+        store=True,
+        compute='_compute_amount',
+        digits=dp.get_precision('Account'))
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        readonly=True,
+        default=lambda self: self.env.user.company_id)
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+        readonly=True,
+        default=lambda self: self.env.user.company_id.currency_id)
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Vendor',
+        required=True,
+        help='Choose vendor for whom will be withholded tax.')
+    origin = fields.Char(string="Source Document")
+
+    @api.depends('quantity', 'unit_amount', 'tax_ids', 'currency_id')
+    def _compute_amount(self):
+        for record in self:
+            record.untaxed_amount = record.unit_amount * record.quantity
+            taxes = record.tax_ids.compute_all(
+                record.unit_amount,
+                record.currency_id,
+                record.quantity,
+                record.product_id,
+                record.employee_id.user_id.partner_id)
+            record.total_amount = taxes.get('total_included')
 
     @api.model
     def create(self, vals):

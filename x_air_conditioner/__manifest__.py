e717e5dee32603b6c5ee5b7157a16418f31d4ef5
kitcharoen poolperm <kitcharoenp@gmail.com>
2016-12-29 16:35:42 +0700

    [NEW] x_air_conditioner : x_air_conditioner module is sub-set of equipment and maintenace. it is separate to only management air condition only.

---
+++ x_air_conditioner/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Air Conditioner - Maintenance',
+    'version': '1.0',
+    'sequence': 125,
+    'description': """
+        Air Conditioner - Maintenance.""",
+    'depends': ['x_maintenance'],
+    'summary': 'Air Conditioner Maintenance',
+    'data': [
+        # Menus and Actions
+        'actions/x_air_conditioner_equipment_act_window.xml',
+        'actions/x_air_conditioner_maintenance_act_window.xml',
+        'views/x_air_conditioner_menu_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto-install': True,
+}

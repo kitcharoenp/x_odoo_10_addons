4ede87db6110a7a60453edac98d7a9d41bd8e097
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-01-11 17:21:06 +0700

    [IMP] x_vehicle_odometer : Added the description field to FleetVehicleOdometer model

---
+++ x_vehicle_odometer/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Enhancement - Vehicle Odometer',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Fleet, Vehicle',
+    'description': """
+        Enhancement Vehicle Odometer
+        * Added a description field to Odometer
+        """,
+    'depends': ['fleet'],
+    'summary': 'Enhancement - Vehicle Odometer',
+    'data': [
+    ],
+    'installable': True,
+    'auto-install': True,
+}
---
+++ x_vehicle_odometer/models/x_vehicle_odometer.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class FleetVehicleOdometer(models.Model):
+    _inherit = 'fleet.vehicle.odometer'
+
+    x_description = fields.Text(string='Description')

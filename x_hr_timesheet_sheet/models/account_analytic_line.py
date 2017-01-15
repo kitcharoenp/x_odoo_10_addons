c7428690ad4c6aafa76de238e269c902a3443108
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-01-15 11:37:31 +0700

    [FIX] x_hr_timesheet_sheet : Qweb-pdf decoration header and table body

--- x_hr_timesheet_sheet/models/account_analytic_line.py
+++ x_hr_timesheet_sheet/models/account_analytic_line.py
@@ -11,7 +11,7 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
-    _order = 'date desc, x_start_date desc'
+    _order = 'date desc, x_start_date asc'
 
     x_vehicle_id = fields.Many2one(
         'fleet.vehicle',

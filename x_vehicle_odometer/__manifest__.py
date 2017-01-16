aac48814a0d33c2a5929844f4f224c975636a382
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-01-16 14:20:59 +0700

    [IMP] x_vehicle_odometer : make rules which user can see all vehicle and odometer, only read/write own odometer record

--- x_vehicle_odometer/__manifest__.py
+++ x_vehicle_odometer/__manifest__.py
@@ -10,6 +10,8 @@ Enhancement Vehicle Odometer :
 -------------
     * Added a description field to Odometer log
     * Added a driver field to Odometer log
+    * Rule fleet/user see all Vehicle and Odometer
+    * Rule user can only modify own records
         """,
     'depends': ['fleet'],
     'summary': 'Enhancement - Vehicle Odometer',
@@ -17,6 +19,7 @@ Enhancement Vehicle Odometer :
         'actions/x_vehicle_odometer_act_window.xml',
         'views/x_vehicle_odometer_form_view.xml',
         'views/x_vehicle_odometer_tree_view.xml',
+        'security/x_vehicle_odometer_security.xml',
     ],
     'installable': True,
     'auto-install': True,

c9d5487e5d5411f7e87a578a57eddf1cf447fb70
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-03-08 10:56:25 +0700

    [IMP] x_maintenance : removed default image attribute in model

--- x_maintenance/models/x_maintenance_equipment.py
+++ x_maintenance/models/x_maintenance_equipment.py
@@ -7,32 +7,21 @@ from odoo.modules.module import get_module_resource
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
-    @api.model
-    def _default_image(self):
-        image_path = get_module_resource(
-            'x_maintenance', 'static/src/img', 'default_image.png')
-        return tools.image_resize_image_big(
-            open(image_path, 'rb').read().encode('base64'))
-
     x_vehicle_id = fields.Many2one(
         'fleet.vehicle',
         string='Vehicle')
     x_asset_number = fields.Char('Asset Number', copy=False)
     image01 = fields.Binary(
         string="Image01",
-        default=_default_image,
         attachment=True)
     image02 = fields.Binary(
         string="Image02",
-        default=_default_image,
         attachment=True)
     image03 = fields.Binary(
         string="Image03",
-        default=_default_image,
         attachment=True)
     image04 = fields.Binary(
         string="Image04",
-        default=_default_image,
         attachment=True)
     product_id = fields.Many2one(
         'product.product',

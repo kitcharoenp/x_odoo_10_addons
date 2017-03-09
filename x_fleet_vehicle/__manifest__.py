69fcc8b0f9e8b6033d9433964a5280b5cbc818c6
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-03-09 09:46:30 +0700

    {IMP] x_fleet_vehicle : enhancement the fleet vehicle module with the addition field

---
+++ x_fleet_vehicle/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Fleet Vehicle',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Fleet',
+    'description': """
+Enhancement Information on a vehicle :
+-------------
+    * First Driver field related to employee
+    * Second driver field related to employee
+    * Maintenance Administrator field related to employee
+        """,
+    'depends': ['fleet'],
+    'summary': 'Enhancement Information on a vehicle',
+    'data': [
+
+    ],
+    'installable': True,
+    'auto-install': False,
+}

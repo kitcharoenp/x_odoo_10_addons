c0462e5f1025ab1e6c61e5fdfe0588919b835f5c
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-03-20 13:03:15 +0700

    [INI] x_report_timesheet_perdiem : initial the perdiem timesheet report module

--- x_report_timesheet_perdiem/__manifest__.py
+++ x_report_timesheet_perdiem/__manifest__.py
@@ -11,8 +11,12 @@
     'summary': 'Timesheet Per Diem Report',
     'data': [
         # Views
+        'views/x_report_timesheet_perdiem_form_view.xml',
         # Menu and Action
+        'actions/x_report_timesheet_perdiem_act_window.xml',        
+        'views/x_report_timesheet_perdiem_menu_view.xml',
         # Report
+        'report/templates/x_report_ts_perdiem_template.xml',
         # Security
     ],
     'installable': True,

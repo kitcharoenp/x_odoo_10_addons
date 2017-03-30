ad56aa2427cf105031e1f29abb89e34149cefb45
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-03-30 10:38:06 +0700

    [IMP] x_fleet_fuel_log : customize search, graph and pivot view

--- x_fleet_fuel_log/__manifest__.py
+++ x_fleet_fuel_log/__manifest__.py
@@ -8,7 +8,7 @@
     'description': """
 Fleet Vehicle Fuel Log :
 -------------
-    * compute cousumption base on mileage and litre
+    * compute fuel cousumption base on mileage and litre
         """,
     'depends': ['fleet'],
     'summary': 'Fleet Vehicle Fuel Log',
@@ -16,7 +16,11 @@ Fleet Vehicle Fuel Log :
         # View
         'views/x_fleet_fuel_log_form_view.xml',
         'views/x_fleet_fuel_log_tree_view.xml',
-        'views/x_fleet_fuel_log_graph_view.xml'
+        'views/x_fleet_fuel_log_graph_view.xml',
+        'views/x_fleet_fuel_log_pivot_view.xml',
+        'views/x_fleet_fuel_log_search_view.xml',
+        # Action
+        'views/x_fleet_fuel_log_action_window.xml',
     ],
     'installable': True,
     'auto-install': False,

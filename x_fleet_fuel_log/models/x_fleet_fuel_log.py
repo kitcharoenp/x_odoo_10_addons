f6f6e5a84466cf0a3c787ad7b4281c47449f8608
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-03-31 16:24:01 +0700

    [IMP] x_fleet_fuel_log : change the x_last_refill_odometer field to x_last_refuel_odometer

--- x_fleet_fuel_log/models/x_fleet_fuel_log.py
+++ x_fleet_fuel_log/models/x_fleet_fuel_log.py
@@ -11,12 +11,26 @@ class FleetVehicleLogFuel(models.Model):
         group_operator="avg",
         string="Fuel Consumption",
         store="True")
-    x_last_refill_odometer = fields.Float(string="Last Refill Odometer")
+    x_last_refuel_odometer = fields.Float(string="Last Refuel Odometer")
 
     @api.multi
-    @api.depends('liter', 'odometer', 'x_last_refill_odometer')
+    @api.depends('liter', 'odometer', 'x_last_refuel_odometer')
     def _compute_fuel_consumption(self):
         for fuel_log in self:
-            distance = fuel_log.odometer - fuel_log.x_last_refill_odometer
+            distance = fuel_log.odometer - fuel_log.x_last_refuel_odometer
             if fuel_log.liter > 0:
                 fuel_log.x_fuel_consumption = distance / fuel_log.liter
+
+    def _get_last_refuel_odometer(self):
+        vehicle_log_fuel = self.env['fleet.vehicle.log.fuel'].search([
+                ('vehicle_id', '=', self.vehicle_id.id)],
+                order="odometer desc",
+                limit=1)
+        return vehicle_log_fuel.odometer
+
+    @api.onchange('vehicle_id')
+    def _onchange_vehicle(self):
+        super(FleetVehicleLogFuel, self)._onchange_vehicle()
+        if self.vehicle_id:
+            x_last_refuel_odometer = self._get_last_refuel_odometer()
+            self.x_last_refuel_odometer = x_last_refuel_odometer

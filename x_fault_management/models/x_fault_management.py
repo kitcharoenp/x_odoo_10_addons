dcc5e2255502de023c618696df87b767704f780d
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-04-30 11:46:08 +0700

    [IMP] x_fault_management : new fields (state_id, latitude, longitude, distance, process_time, temporarily, permanant )

--- x_fault_management/models/x_fault_management.py
+++ x_fault_management/models/x_fault_management.py
@@ -4,6 +4,8 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 import odoo.addons.decimal_precision as dp
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
 
 
 class XFaultManagement(models.Model):
@@ -19,7 +21,7 @@ class XFaultManagement(models.Model):
         index=True,
         copy=False,
         default=lambda self: _('New'))
-    customer_reference = fields.Char(string="Customer Reference/ID")
+    customer_reference = fields.Char(string="Reference/ID")
     partner_id1 = fields.Many2one(
         'res.partner',
         string='Customer A')
@@ -32,6 +34,33 @@ class XFaultManagement(models.Model):
         string="End DateTime")
     cause_of_fault = fields.Text('Cause of Fault')
     troubleshooting = fields.Text('Troubleshooting')
+    duration = fields.Float(
+        string='Duration',
+        compute='_compute_duration',
+        store=True,
+        readonly=True)
+    state_id = fields.Many2one(
+        "res.country.state",
+        string='State')
+    latitude = fields.Float(string='Geo Latitude', digits=(16, 5))
+    longitude = fields.Float(string='Geo Longitude', digits=(16, 5))
+    distance = fields.Float(string='Distance')
+    permanant = fields.Boolean(default=False)
+    temporarily = fields.Boolean(default=False)
+    process_time = fields.Float(string='Process Time')
+
+    @api.depends('start_datetime', 'end_datetime')
+    def _compute_duration(self):
+        for fault_management in self:
+            if fault_management.end_datetime:
+                end_dt = datetime.strptime(
+                    fault_management.end_datetime,
+                    DEFAULT_SERVER_DATETIME_FORMAT)
+                start_dt = datetime.strptime(
+                    fault_management.start_datetime,
+                    DEFAULT_SERVER_DATETIME_FORMAT)
+                delta = end_dt - start_dt
+                fault_management.duration = delta.total_seconds() / 60.0
 
     @api.model
     def create(self, vals):

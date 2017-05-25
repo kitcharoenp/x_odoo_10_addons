96f41ee60986539c87e78d853da8bede1dd331aa
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-05-25 16:20:05 +0700

    [IMP] telco_work_order : fix import odoo.addons.cmis_field.fields

--- telco_work_order/models/telco_work_order_cmis.py
+++ telco_work_order/models/telco_work_order_cmis.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models
-from odoo.addons.cmis_field import fields
+from odoo.addons.cmis_field.fields import CmisFolder
 
 
 class TelcoWorkOrderCmis(models.Model):
     _inherit = 'telco.work.order'
 
-    cmis_folder = fields.CmisFolder()
+    cmis_folder = CmisFolder()

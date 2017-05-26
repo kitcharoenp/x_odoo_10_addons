fe2dd0421ae4d4d4d6b3f643c879dca3ee1bdbab
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-05-26 17:23:13 +0700

    [NEW] x_hr_timesheet_security : grant access model write on resource and employee for employee group

---
+++ x_hr_timesheet_security/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Timesheet Records/Fields Security',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    "author": "kicharoenp@gmail.com",
+    'description': """
+Timesheet Records/Fields Security
+-------------
+    * Grant access create on model_hr_employee to base.group_user
+    * Grant access write on model_resource_resource to base.group_user
+    * Grant access write on employee holidays rule to base.group_user
+        """,
+    'depends': [
+        'hr',
+        'resource',
+        'x_hr_timesheet_sheet',
+        'hr_holidays',
+    ],
+    'summary': """
+        Timesheet Records/Fields Security
+        """,
+    'data': [
+        # Security
+        'security/ir.model.access.csv',
+        'security/x_hr_timesheet_security.xml',
+        # Menu and Action
+        # Views
+    ],
+    'installable': True,
+    'auto-install': False,
+}

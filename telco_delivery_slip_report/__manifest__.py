0a814d6eb893b56ab27c00cb7ca1297df705573b
kitcharoen poolperm <kitcharoenp@gmail.com>
2017-08-28 13:08:25 +0700

    [NEW] telco_delivery_slip_report : customize delivery slip report

---
+++ telco_delivery_slip_report/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @author Kitcharoen Poolperm <kitcharoenp@gmail.com>
+# @copyright Copyright (C) 2017
+# @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
+{
+    'name': 'Telco Delivery Slip Report',
+    'version': '0.01',
+    'category': 'Warehouse',
+    "author": "kicharoenp@gmail.com",
+    'summary': 'Telco Delivery Slip Report',
+    'description': """
+    """,
+    'depends': ['stock', ],
+    'data': [
+        # Views
+        # Reports and Templates
+        'report/templates/telco_report_deliveryslip_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}

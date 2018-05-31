60b16bd0e1b64d49e7c9a7809e8fd209038254f8
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2018-05-31 06:31:23 +0000

    [IMP] x_fleet_vehicle : on change the x_driver_id1 update driver

--- x_fleet_vehicle/models/x_fleet_vehicle.py
+++ x_fleet_vehicle/models/x_fleet_vehicle.py
@@ -16,3 +16,8 @@ class FleetVehicle(models.Model):
     x_administrator_id = fields.Many2one(
         'hr.employee',
         string='Administrator')
+
+    @api.onchange('x_driver_id1')
+    def _onchange_x_driver(self):
+        if self.x_driver_id1:
+            self.driver_id = self.x_driver_id1.user_id.partner_id

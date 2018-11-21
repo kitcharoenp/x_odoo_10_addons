7745a7c4b2b12f32565ba1f1c3b7c390443248dd
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2018-11-21 05:59:16 +0000

    [IMP] x_fleet_vehicle:model : add a new field (x_fleet_card_no, x_fleet_card_password) to model

--- x_fleet_vehicle/models/x_fleet_vehicle.py
+++ x_fleet_vehicle/models/x_fleet_vehicle.py
@@ -16,8 +16,20 @@ class FleetVehicle(models.Model):
     x_administrator_id = fields.Many2one(
         'hr.employee',
         string='Administrator')
+    x_fleet_card_no = fields.Char('Fleet Card No.', copy=False)
+    x_fleet_card_password = fields.Char('Fleet Card Password', copy=False)
 
     @api.onchange('x_driver_id1')
     def _onchange_x_driver(self):
         if self.x_driver_id1:
             self.driver_id = self.x_driver_id1.user_id.partner_id
+
+    def _get_odometer(self):
+        super(FleetVehicle, self)._get_odometer()
+        FleetVehicalOdometer = self.env['fleet.vehicle.odometer']
+        for record in self:
+            vehicle_odometer = FleetVehicalOdometer.search([('vehicle_id', '=', record.id)], limit=1, order='value desc')
+            if vehicle_odometer:
+                record.odometer = vehicle_odometer.y_odometer
+            else:
+                record.odometer = 0

b5829be550e3a015ee1380ae09c0a11e80c1b9e1
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2019-02-09 07:06:42 +0000

    [IMP] telco_account_budget : new the compute fields

--- telco_account_budget/models/telco_account_budget.py
+++ telco_account_budget/models/telco_account_budget.py
@@ -10,49 +10,62 @@ class CrossoveredBudgetLines(models.Model):
     _inherit = 'crossovered.budget.lines'
 
     x_internal_reference = fields.Char('Internal Reference',)
-    percentage = fields.Float(
-        compute='_compute_percentage',
-        string='Achievement',
-        group_operator="avg",)
+    x_practical_amount = fields.Float(
+        compute='_compute_x_practical_amount',
+        group_operator="sum",
+        store="True",
+        string='Post Amount', digits=0)
     available_amount = fields.Float(
-        compute='_compute_available_amount',
+        compute='_compute_x_available_amount',
         string='Available Amount',
         group_operator="sum",
         store="True")
-    x_practical_amount = fields.Float(
-        compute='_compute_practical_amount',
+    x_available_amount = fields.Float(
+        compute='_compute_x_available_amount',
+        string='Available Amount',
         group_operator="sum",
-        store="True",
-        string='Practical Amount', digits=0)
+        store="True")
     x_percentage = fields.Float(
         compute='_compute_x_percentage',
         group_operator="avg",
         store="True",
-        string='Achievement')
-
+        string='Availability %')
 
     @api.multi
-    def _compute_available_amount(self):
+    @api.depends('general_budget_id', 'date_to', 'date_from', 'analytic_account_id')
+    def _compute_x_practical_amount(self):
+        for line in self:
+            result = 0.0
+            acc_ids = line.general_budget_id.account_ids.ids
+            if not acc_ids:
+                raise UserError(_("The Budget '%s' has no accounts!") % ustr(line.general_budget_id.name))
+            date_to = self.env.context.get('wizard_date_to') or line.date_to
+            date_from = self.env.context.get('wizard_date_from') or line.date_from
+            if line.analytic_account_id.id:
+                self.env.cr.execute("""
+                    SELECT SUM(amount)
+                    FROM account_analytic_line
+                    WHERE account_id=%s
+                        AND (date between to_date(%s,'yyyy-mm-dd') AND to_date(%s,'yyyy-mm-dd'))
+                        AND general_account_id=ANY(%s)""",
+                (line.analytic_account_id.id, date_from, date_to, acc_ids,))
+                result = self.env.cr.fetchone()[0] or 0.0
+            line.x_practical_amount = result
+
+    @api.multi
+    @api.depends('planned_amount', 'x_practical_amount')
+    def _compute_x_available_amount(self):
         for line in self:
             if line.planned_amount != 0.00:
-                line.available_amount = float((line.planned_amount or 0.0) - line.practical_amount)
+                line.x_available_amount = float((line.planned_amount or 0.0) - line.x_practical_amount)
             else:
-                line.available_amount = 0.00
+                line.x_available_amount = 0.00
 
     @api.multi
-    #@api.depends('general_budget_id', 'analytic_account_id', 'date_from', 'date_to')
-    @api.depends('practical_amount',)
-    def _compute_practical_amount(self):
-        super(CrossoveredBudgetLines, self)._compute_practical_amount()
-        for line in self:
-            if line.practical_amount:
-                line.x_practical_amount = line.practical_amount
-
-    @api.multi
-    @api.depends('practical_amount', 'theoritical_amount')
+    @api.depends('planned_amount', 'x_available_amount')
     def _compute_x_percentage(self):
         for line in self:
-            if line.theoritical_amount != 0.00:
-                line.x_percentage = float((line.practical_amount or 0.0) / line.theoritical_amount) * 100
+            if line.planned_amount != 0.00:
+                line.x_percentage = float((line.x_available_amount or 0.0) / line.planned_amount) * 100
             else:
                 line.x_percentage = 0.00
